7a33a807ed6c13a4ab80bddc78c6561850b18cc0
doraemon4324 <47148721+doraemon4324@users.noreply.github.com>
2020-07-05 22:36:01 +0700

    Send notification API Test
    With Newtonsoft Nuget package install

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using DWDW_API.Core.Constants;
 using DWDW_API.Providers;
 using DWDW_Service.Repositories;
+using DWDW_Service.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DWDW_API.Controllers
 {
@@ -37,6 +41,35 @@ namespace DWDW_API.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet]
+        [Route("TestNotify")]
+        public IActionResult TestSendNotify(string room)
+        {
+            IActionResult result = Ok();
+
+            string deviceToken = "cAF8JeveS9av5pIdQtge0-:APA91bGvzkAno7ycM_fIzqwEjhIUTBy-la9u71_" +
+                "vYocHFhnnuGIO0PyfAMU2ph0cae6YuRGpYTAnbw9KtcKgN-aENmED3Bz4KLHnjrpU9HgfRHhBcTBP_" +
+                "gbd41-tcsMD4kC9Vl0dnHC2";
+            string now = DateTime.Now.ToString("H:mm");
+
+            string titleText = "Detect drowsiness!";
+            string bodyText = "There was a drowsiness in " + room  + " at " + now;
+            var data = new
+            {
+                notification = new
+                {
+                    title = titleText,
+                    body = bodyText
+                },
+                to = deviceToken
+            };
+            string xc = JsonConvert.SerializeObject(data);
+            byte[] byteArray = Encoding.UTF8.GetBytes(xc);
+            TestService testService = new TestService();
+            testService.SendNotification(byteArray);
+            return result;
         }
 
     }
