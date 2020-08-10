bd856fee1a8f14921c729deab2290f0d14223dfc
chi_nguyen <47129082+chinguyen9@users.noreply.github.com>
2020-08-11 05:36:39 +0700

    xoa de up lai

--- DWDW_Database/dbo/Tables/Device.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Device] (
-    [deviceId]   INT           IDENTITY (1, 1) NOT NULL,
-    [deviceCode] NVARCHAR (50) NULL,
-    [isActive]   BIT           NULL,
-    PRIMARY KEY CLUSTERED ([deviceId] ASC)
-);
-
-
-
--- DWDW_Database/dbo/Tables/Role.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Role] (
-    [roleId]   INT           IDENTITY (1, 1) NOT NULL,
-    [roleName] NVARCHAR (50) NULL,
-    [isActive] BIT           NULL,
-    PRIMARY KEY CLUSTERED ([roleId] ASC)
-);
-
